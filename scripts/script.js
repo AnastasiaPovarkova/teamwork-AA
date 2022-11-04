03c3859d54debccbe7ff954688c867d17cd1cbae
Andrey <andreas42r@yandex.ru>
2022-11-04 12:28:11 +0700

    places modal images

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,20 @@
 let headerNav = document.querySelector(".header__nav");
 let burger = document.querySelector(".burger");
 let page = document.querySelector(".page");
+let placesImgs = document.querySelectorAll(".places__image");
 
+/* Burger */
 burger.addEventListener("click", (event) => {
   event.preventDefault();
 
   burger.classList.toggle("burger_active");
   headerNav.classList.toggle("header__nav_active");
-  page.classList.toggle("page_no-scroll")
+  page.classList.toggle("page_no-scroll");
+})
+
+placesImgs.forEach((img) => {
+  img.addEventListener("click", () => {
+    img.classList.toggle("places__image_active");
+    page.classList.toggle("page_no-scroll");
+  })
 })
